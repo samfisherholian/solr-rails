a3d7be01aa3917cf921fbba7ec17c6faefdefa8d
Rosivaldo Lucas <53700136+Rosivaldo-Lucas@users.noreply.github.com>
2023-11-04 07:19:08 -0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@
 Rails.application.routes.draw do
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :users do
+    collection do
+      post :search  # creates a route called users_search
+    end
+  end
+  
 end
